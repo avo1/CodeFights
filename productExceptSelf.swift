2313b9bc9a7cc5c48f22ff498b82692761e37266
avo1 <voanhdung@gmail.com>
2017-03-30 20:47:09 +0700

    productExceptSelf

---
+++ productExceptSelf.swift
@@ -0,0 +1,35 @@
+// https://codefights.com/interview/Bpdq6EYZ6MW586va3
+// Amazon, Linked In, Fb... 3000
+
+func productExceptSelf(nums: [Int], m: Int) -> Int {
+    let a = nums.map { $0 % m }
+    
+    // flag[i] = (product when exclude i) % m 
+    // NOTE: (exclude i, not a[i])
+    // because a[i] <= 100, 
+    // so flag should have maximum 100 elements
+
+    var flag: [Int:Int] = [:]
+    var g = 0, f = 0
+    
+    for i in 0..<a.count {
+        f = 1
+        if let k = flag[a[i]] {
+            g += k
+        } else {
+            for j in 0..<a.count {
+                if i != j {
+                    f *= (a[j] % m)
+                    f %= m
+                }
+            }
+            g += f%m
+            flag[a[i]] = f%m
+        }
+        
+        //print(f, g)
+    }
+
+    return g % m
+}
+
