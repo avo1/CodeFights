d7763d4c9287a5f3117a50987ec61facd1ae13da
newbision <slizr88@gmail.com>
2022-05-30 13:48:12 +0300

    Update isLucky.swift

--- Arcade/Intro/isLucky.swift
+++ Arcade/Intro/isLucky.swift
@@ -1,10 +1,11 @@
 // https://codefights.com/arcade/intro/level-3/3AdBC97QNuhF6RwsQ
 
 func isLucky(n: Int) -> Bool {
-    var a = String(n).characters.map { Int("\($0)")! }, h = a.count / 2
-    var h1 = a[0..<h].reduce(0, combine: +)
-    var h2 = a[h..<2*h].reduce(0, combine: +)
-    
+    let a = String(n).map { Int("\($0)")! }, h = a.count / 2
+    let h1 = a[0..<h].reduce(0) {
+        $0 + $1
+    }
+    let h2 = a[h..<2*h].reduce(0) {$0 + $1} 
     print(a)
     return h1 == h2
 }
